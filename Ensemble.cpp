652eb79278c391120536a8bab24a1ca6ba3dabca
Arkmut <Arkmut@users.noreply.github.com>
2016-10-21 15:13:45 +0200

    Add files via upload

---
+++ Ensemble.cpp
@@ -0,0 +1,65 @@
+/*************************************************************************
+                           ${file_base}  -  description
+                             -------------------
+    début                : ${date}
+    copyright            : (C) ${year} par ${user}
+*************************************************************************/
+
+//---------- Réalisation de la classe Ensemble -------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Ensemble.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type ${file_base}::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+
+Ensemble::Ensemble (unsigned int cMax=CARD_MAX )
+	:cardMax(cMax),cardAct(0)
+// Algorithme :
+//
+{
+	#ifdef MAP
+		cout << "Appel au constructeur de <Ensemble>" << endl;
+	#endif
+	tableau=new int[cardMax];
+
+
+
+} //----- Fin de Ensemble
+
+
+Ensemble::~Ensemble( )
+// Algorithme :
+//
+{
+	#ifdef MAP
+		cout << "Appel au destructeur de <Ensemble>" << endl;
+	#endif
+	delete [] tableau;
+} //----- Fin de ~Ensemble
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
