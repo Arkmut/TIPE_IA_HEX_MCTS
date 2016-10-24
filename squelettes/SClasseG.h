d8195b75f099c71197bddabc8b708f62f61a90a0
sjindun <sjindun@gmail.com>
2016-10-24 14:56:37 +0200

    Add files via upload
    test2 coucou coucou

---
+++ squelettes/SClasseG.h
@@ -0,0 +1,89 @@
+/*************************************************************************
+                           ${file_base}  -  description
+                             -------------------
+    début                : ${date}
+    copyright            : (C) ${year} par ${user}
+*************************************************************************/
+
+//---------- Interface de la classe <${file_base}> (fichier ${file_name}) ------
+#if ! defined ( ${include_guard_symbol} )
+#define ${include_guard_symbol}
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <${file_base}>
+//
+//
+//------------------------------------------------------------------------ 
+
+class ${file_base} : public Ancetre
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste de paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    ${file_base} & operator = ( const ${file_base} & un${file_base} );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    ${file_base} ( const ${file_base} & un${file_base} );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    ${file_base} ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~${file_base} ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <${file_base}>
+
+#endif // ${include_guard_symbol}
+
---
+++ src/Ensemble.cpp
@@ -0,0 +1,143 @@
+/*************************************************************************
+                           ${file_base}  -  description
+                             -------------------
+    début                : ${date}
+    copyright            : (C) ${year} par ${user}
+*************************************************************************/
+
+//---------- Réalisation de la classe Ensemble -------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Ensemble.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+void Ensemble::Afficher()
+// Algorithme :
+{
+	cout<<cardAct<<"\r\n";
+	cout<<cardMax<<"\r\n";
+	cout<<'{';
+	for(unsigned int i = 0;i<cardAct;i++){
+		cout<<tableau[i];
+		if(i!=cardAct-1){
+			cout<<',';
+		}
+	}
+	cout<<'}'<<"\r\n";
+}//----- Fin de Méthode
+// type ${file_base}::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+
+Ensemble::Ensemble (unsigned int cMax)
+	:cardMax(cMax),cardAct(0)
+// Algorithme :
+//
+{
+	#ifdef MAP
+		cout << "Appel au constructeur de <Ensemble>" << endl;
+	#endif
+	tableau=new int[cardMax];
+
+
+
+} //----- Fin de Ensemble
+
+Ensemble::Ensemble(int t[],unsigned int nbElements)
+	:cardMax(nbElements),cardAct(nbElements)
+// Algorithme :
+//
+{
+	#ifdef MAP
+		cout << "Appel au constructeur de <Ensemble>" << endl;
+	#endif
+	tableau=new int[cardMax];
+	for(unsigned int i = 0;i<cardAct;i++){
+		tableau[i]=t[i];
+	}
+	
+	quicksort(0,cardAct);
+	int old=tableau[0];
+	for(unsigned int i = 1;i<cardAct;i++){
+		int actual=tableau[i];
+		if(old==actual){
+			shift(i,-1);
+			cardAct--;
+			i--;
+		}else if(old<actual){
+			old=actual;
+		}
+	}
+
+
+} //----- Fin de Ensemble
+
+
+Ensemble::~Ensemble( )
+// Algorithme :
+//
+{
+	#ifdef MAP
+		cout << "Appel au destructeur de <Ensemble>" << endl;
+	#endif
+	delete [] tableau;
+} //----- Fin de ~Ensemble
+
+
+//------------------------------------------------------------------ PRIVE
+
+void Ensemble::quicksort(int debut,int fin)
+{
+	if(debut<fin){
+		int pos=partition(debut,fin);
+		quicksort(debut,pos);
+		quicksort(pos+1,fin);
+	}
+}
+int Ensemble::partition(int debut,int fin)
+{
+	int swap=0;
+	int pivotIndex=debut;
+	for(int i = debut+1;i<fin;i++)
+	{
+		if(tableau[i]<=tableau[debut]){
+			pivotIndex++;
+			swap=tableau[pivotIndex];
+			tableau[pivotIndex]=tableau[i];
+			tableau[i]=swap;
+			
+		}
+	}
+	swap=tableau[pivotIndex];
+	tableau[pivotIndex]=tableau[debut];
+	tableau[debut]=swap;
+	return pivotIndex;
+}
+void Ensemble::shift(unsigned int start, int direction)
+{
+	for(unsigned int i = start;i<cardAct;i++){
+		int temp=tableau[i+direction];
+		tableau[i+direction]=tableau[i];
+		tableau[i]=temp;
+	}
+}
+//----------------------------------------------------- Méthodes protégées
+
