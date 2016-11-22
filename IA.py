db9ff211952a3f9f9cf0d3ddd5f4299bb9906513
Piandow <pakoyaaki@gmail.com>
2016-11-22 16:17:25 +0100

    Changement avec les arbres
    Renvoi des coups possibles sous forme d'arbre, bien plus clair que des listes de liste de liste...

--- IA.py
+++ IA.py
@@ -1,33 +1,31 @@
+#Crée un arbre de racine plateau et de fils les coups possibles en un coup
 def coupsPossibles(plateau):
-    coups = []
+    coups = Arbre(plateau)
     for i in range (plateau.taille):
         for j in range(plateau.taille):
             if (plateau.mat[i,j] == 0):
-                coups.append([i,j])
+                coups.ajout([i,j])
     return coups
 
-def deepcopy(plateau):
-    plat = Plateau(plateau.taille)
-    for i in range(plateau.taille):
-        for j in range(plateau.taille):
-            plat.mat[i,j] = plateau.mat[i,j]
-    return plat
+#On garde deepCopy sous le coude au cas où, mais on en a plus besoin ici maintenant qu'on utilise les arbres
+#def deepcopy(plateau):
+#    plat = Plateau(plateau.taille)
+#    for i in range(plateau.taille):
+#        for j in range(plateau.taille):
+#            plat.mat[i,j] = plateau.mat[i,j]
+#    return plat
 
-def feuille(plateau, nbEtage, joueur):
-    if (nbEtage == 1):
-        l = coupsPossibles(plateau)
-        coups = []
-        for k in l:
-            coups.append([k])
-        return coups
-    else:
-        l = coupsPossibles(plateau)
-        coups = []
-        for k in l:
-            plat = deepcopy(plateau)
-            plat.mat[k[0], k[1]] = joueur
-            coupsK = feuille(plat, nbEtage - 1, 3 - joueur)
-            for m in coupsK:
-                m.insert(0, k) 
-            coups.extend(coupsK)
-        return coups
+#Crée un arbre de racine plateau qui représente toutes les séries de nbEtage coups
+def arbreCoups(plateau, nbEtage, joueur):
+    abr = coupsPossibles(plateau)
+    def aux(arbre, n, j):
+        if (n == 0):
+            return arbre
+        else:
+            for k in range(len(arbre.fils)):
+                arbre.fils[k] = Arbre(arbre.fils[k])
+                arbre.fils[k].fils = arbre.fils[:k] + arbre.fils[k+1:]
+                aux(arbre.fils[k], n - 1, 3 - j)
+            return arbre
+    return aux(abr, nbEtage - 1, joueur)
+#joueur n'a pour l'instant pas d'utilité ici
