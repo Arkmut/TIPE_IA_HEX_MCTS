02f668ae8bfc1bcc0d1fc17b39a291185292386a
Kalderian <kalderian.drialsinnis@gmail.com>
2016-12-31 15:16:13 +0100

    Au revoir Numpy
    On passe à des matrices normales

--- HEX.py
+++ HEX.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 class Plateau:
@@ -8,11 +7,14 @@ class Plateau:
 
     def __init__(self, largeur):
         self.taille = largeur
-        self.mat = np.zeros((self.taille, self.taille))
+        self.mat = [[]] * self.taille
+        for i in range(self.taille):
+            self.mat[i] = [0] * self.taille
+        
     
     def joue(self, joueur, i, j):
-        if(self.mat[i,j] == 0):
-            self.mat[i,j] = joueur
+        if(self.mat[i][j] == 0):
+            self.mat[i][j] = joueur
             return True
         else:
             return False
@@ -21,7 +23,7 @@ class Plateau:
         if(nAffichage == 1):
             s = ""
             for i in range(0, self.taille):
-                s += str(i) + "   "
+                s += str(i) + "  "
             print("  " * self.taille + "x\y", s)
             for i in range(0, self.taille):
                 print("  " * (self.taille - i - 1), i, self.mat[i])
@@ -65,19 +67,19 @@ class Plateau:
                 i += 1
             #On applique récursivement chemin à chaque case de la liste contenant un pion de joueur
             for i in range(0, len(ptsAdja)):
-                if((self.mat[ptsAdja[i][0], ptsAdja[i][1]] == joueur) and (self.chemin(joueur, ptsAdja[i][0], ptsAdja[i][1], dejaVu))):
-                        return True
+                if((self.mat[ptsAdja[i][0]][ptsAdja[i][1]] == joueur) and (self.chemin(joueur, ptsAdja[i][0], ptsAdja[i][1], dejaVu))):
+                    return True
             return False
         
     def checkVictoire(self, joueur):
         sortie = False
         if(joueur == 1):
             for i in range(0, self.taille):
-                if(self.mat[i,0] == joueur) and not(sortie):
+                if(self.mat[i][0] == joueur) and not(sortie):
                     sortie = self.chemin(joueur, i, 0, [])
         if(joueur == 2):
             for i in range(0, self.taille):
-                if(self.mat[0,i] == joueur) and not(sortie):
+                if(self.mat[0][i] == joueur) and not(sortie):
                     sortie = self.chemin(joueur, 0, i, [])
         return sortie
 
@@ -91,11 +93,11 @@ while(pasDeGagnant):
     marchePas = True
     while(marchePas):
         if joueur == 1:
-            x = input("Xjoueur1 = ")
-            y = input("Yjoueur1 = ")
+            x = int(input("Xjoueur1 = "))
+            y = int(input("Yjoueur1 = "))
         elif joueur == 2:
-            x = input("Xjoueur2 = ")
-            y = input("Yjoueur2 = ")
+            x = int(input("Xjoueur2 = "))
+            y = int(input("Yjoueur2 = "))
         marchePas = not(plateau.joue(joueur, x, y))
     pasDeGagnant = not(plateau.checkVictoire(joueur))
     if(pasDeGagnant):
