f3809934163658069095ad3f6ecb2c68742c852d
Kalderian <kalderian.drialsinnis@gmail.com>
2017-02-01 13:47:27 +0100

    Update and rename HEX.py to plateau.py

--- plateau.py
+++ plateau.py
@@ -82,26 +82,4 @@ class Plateau:
                 if(self.mat[0][i] == joueur) and not(sortie):
                     sortie = self.chemin(joueur, 0, i, [])
         return sortie
-
-##
-
-plateau = Plateau(9)
-joueur = 1
-pasDeGagnant = True
-
-while(pasDeGagnant):
-    marchePas = True
-    while(marchePas):
-        if joueur == 1:
-            x = int(input("Xjoueur1 = "))
-            y = int(input("Yjoueur1 = "))
-        elif joueur == 2:
-            x = int(input("Xjoueur2 = "))
-            y = int(input("Yjoueur2 = "))
-        marchePas = not(plateau.joue(joueur, x, y))
-    pasDeGagnant = not(plateau.checkVictoire(joueur))
-    if(pasDeGagnant):
-        joueur = 3 - joueur
-    plateau.affiche(1)
-    
-print("joueur n°" + str(joueur) + " gagne")
+  
