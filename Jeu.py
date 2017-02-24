7ef0fa68cd1bd485cf7e7c4e8edc7b5bcb2583a1
Kalderian <kalderian.drialsinnis@gmail.com>
2017-02-24 10:58:07 +0100

    prise en compte de l'ia

--- Jeu.py
+++ Jeu.py
@@ -1,18 +1,40 @@
-plateau = Plateau(9)
+plateau = Plateau(5)
 joueur = 1
 pasDeGagnant = True
+arbreCoups = initialisation(plateau)
+
+# while(pasDeGagnant):
+#     marchePas = True
+#     while(marchePas):
+#         if joueur == 1:
+#             x = int(input("Xjoueur1 = "))
+#             y = int(input("Yjoueur1 = "))
+#         elif joueur == 2:
+#             x = int(input("Xjoueur2 = "))
+#             y = int(input("Yjoueur2 = "))
+#         marchePas = not(plateau.joue(joueur, x, y))
+#     pasDeGagnant = not(plateau.checkVictoire(joueur))
+#     if(pasDeGagnant):
+#         joueur = 3 - joueur
+#     plateau.affiche(1)
 
 while(pasDeGagnant):
     marchePas = True
+    x = -1
+    y = -1
     while(marchePas):
         if joueur == 1:
-            x = int(input("Xjoueur1 = "))
-            y = int(input("Yjoueur1 = "))
+            arbreCoups = mcts(arbreCoups)
+            x = arbreCoups.racine[0][0]
+            y = arbreCoups.racine[0][1]
+            arbreCoups.racine[0] = plateau
         elif joueur == 2:
             x = int(input("Xjoueur2 = "))
             y = int(input("Yjoueur2 = "))
         marchePas = not(plateau.joue(joueur, x, y))
     pasDeGagnant = not(plateau.checkVictoire(joueur))
+    if joueur == 2:
+        arbreCoups = rechercheCoup(arbreCoups, x, y, plateau)
     if(pasDeGagnant):
         joueur = 3 - joueur
     plateau.affiche(1)
