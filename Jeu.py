9bdb15de636f3f664fe184f657b27ef4917f3917
Piandow <pakoyaaki@gmail.com>
2017-03-11 19:01:56 +0100

    Commentaires
    Des commentaires ont été ajoutés.

--- Jeu.py
+++ Jeu.py
@@ -19,10 +19,11 @@ arbreCoups = initialisation(plateau)
 #     plateau.affiche(1)
 
 while(pasDeGagnant):
-    marchePas = True
+    CaseOccupée = True
     x = -1
     y = -1
     while(marchePas):
+'''On laisse pour l'instant l'IA jouer en premier. Elle execute l'algorithme MCTS et joue les coups dans la racine de l'arbre renvoyé. On insere ensuite le plateau à la place de cette racine, pour de futures simulations'''
         if joueur == 1:
             arbreCoups = mcts(arbreCoups)
             x = arbreCoups.racine[0][0]
@@ -31,9 +32,9 @@ while(pasDeGagnant):
         elif joueur == 2:
             x = int(input("Xjoueur2 = "))
             y = int(input("Yjoueur2 = "))
-        marchePas = not(plateau.joue(joueur, x, y))
+        CaseOccupée = not(plateau.joue(joueur, x, y))
     pasDeGagnant = not(plateau.checkVictoire(joueur))
-    if joueur == 2:
+    if joueur == 2: #On actualise l'arbre de recherche de l'IA avec le coup de l'adversaire
         arbreCoups = rechercheCoup(arbreCoups, x, y, plateau)
     if(pasDeGagnant):
         joueur = 3 - joueur
