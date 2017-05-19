11864e1784f06b83abcbba48e349555137806461
Piandow <pakoyaaki@gmail.com>
2017-05-19 11:36:33 +0200

    Affichage graphique
    -pygame = on voit enfin ce qu'on joue.

--- Jeu.py
+++ Jeu.py
@@ -1,4 +1,6 @@
-plateau = Plateau(5)
+# -*- coding: utf-8 -*-
+import pygame 
+plateau = Plateau(9, [], (-1,-1))
 joueur = 1
 pasDeGagnant = True
 arbreCoups = initialisation(plateau)
@@ -12,33 +14,39 @@ arbreCoups = initialisation(plateau)
 #         elif joueur == 2:
 #             x = int(input("Xjoueur2 = "))
 #             y = int(input("Yjoueur2 = "))
-#         marchePas = not(plateau.joue(joueur, x, y))
+#         marchePas = not(plateau.joue(joueur, (x, y)))
 #     pasDeGagnant = not(plateau.checkVictoire(joueur))
 #     if(pasDeGagnant):
 #         joueur = 3 - joueur
 #     plateau.affiche(1)
 
-while(pasDeGagnant):
-    CaseOccupée = True
-    x = -1
-    y = -1
-    while(caseOccupee):
 '''On laisse pour l'instant l'IA jouer en premier. Elle execute l'algorithme MCTS et joue le coup dans la racine de l'arbre renvoyé. 
 On insere ensuite le plateau à la place de cette racine, pour de futures simulations'''
+while(pasDeGagnant):
+    caseOccupee = True
+    while(caseOccupee):
         if joueur == 1:
-            arbreCoups = mcts(arbreCoups)
-            x = arbreCoups.racine[0][0]
-            y = arbreCoups.racine[0][1]
+            coup = reaction(plateau, 1)
+            if plateau.coup != coup:
+                print("reaction")
+                arbreCoups = rechercheCoup(arbreCoups, coup, plateau)
+                mcts2(arbreCoups,plateau)
+                (x, y) = coup
+            else:
+                arbreCoups = mcts(arbreCoups, plateau)
+                x = arbreCoups.racine[0][0]
+                y = arbreCoups.racine[0][1]
             arbreCoups.racine[0] = plateau
         elif joueur == 2:
             x = int(input("Xjoueur2 = "))
             y = int(input("Yjoueur2 = "))
-        caseOccupee = not(plateau.joue(joueur, x, y))
+        caseOccupee = not(plateau.joue(joueur, (x, y)))
     pasDeGagnant = not(plateau.checkVictoire(joueur))
     if joueur == 2: #On actualise l'arbre de recherche de l'IA avec le coup de l'adversaire
-        arbreCoups = rechercheCoup(arbreCoups, x, y, plateau)
+        arbreCoups = rechercheCoup(arbreCoups, coup, plateau)
     if(pasDeGagnant):
         joueur = 3 - joueur
-    plateau.affiche(1)
-    
+    plateau.affiche2()
+
+pygame.quit()  
 print("joueur n°" + str(joueur) + " gagne")
