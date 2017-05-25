23a4a9c5542034d151a7359459297ed5a2016178
Kalderian <kalderian.drialsinnis@gmail.com>
2017-05-25 19:58:28 +0200

    Update Neurones.py

--- Neurones.py
+++ Neurones.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from math import exp
+import time
 
 class Neurone:
     
     predecesseur = np.array([])
     poids = np.array([])
     biais = 0
+    sigmoide = lambda x: x
  
     def __init__(self, listePredecesseur, listePoids, biais):
         self.predecesseur = listePredecesseur
         self.poids = listePoids
-        self.biais = biais   
+        self.biais = biais
+        self.sigmoide = np.vectorize(lambda x: 1 / (1 + exp(- x - self.biais)))
 
     def evalue(self, data):
         entree = [self.predecesseur[0].evalue(data)]
         for k in range(1, len(self.predecesseur)):
             np.concatenate((entree, [self.predecesseur[k].evalue(data)]))
         sortie = np.dot(self.poids, entree)
-        s = np.vectorize(lambda x: 1 / (1 + exp(- x - self.biais)))
-        sortie = s(sortie)
+        sortie = self.sigmoide(sortie)
         return sortie
 
 class NeuroneAlpha:
@@ -30,11 +32,12 @@ class NeuroneAlpha:
     def __init__(self, listePoids, biais):
         self.poids = listePoids
         self.biais = biais
+        self.sigmoide = np.vectorize(lambda x: 1 / (1 + exp(- x - self.biais)))
+
 
     def evalue(self, data):
         sortie = np.dot(data, self.poids)
-        s = np.vectorize(lambda x: 1 / (1 + exp(- x - self.biais)))
-        sortie = s(sortie)
+        sortie = self.sigmoide(sortie)
         return sortie
 
 tmps1=time.clock()
